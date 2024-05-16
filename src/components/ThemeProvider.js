f7443fbdbd6d535f7c9d3eb85746a1419446813c
PineberryCode <applepie090902@gmail.com>
2024-05-16 13:40:59 -0500

    Updates

--- src/components/ThemeProvider.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react"
-import { ThemeProvider as ReactThemesProvider } from "next-themes"
-
-export function ThemeProvider({ children, ...props }) {
-    return <ReactThemesProvider {...props}>{children}</ReactThemesProvider>
-}
